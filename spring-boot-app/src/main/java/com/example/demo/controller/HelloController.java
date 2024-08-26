b3bdc27beaccd6107eaa0b8098bd5b5f7aafe09d
Richard <richardlwhittington@gmail.com>
2024-08-25 21:02:46 -0500

    inital commit

---
+++ spring-boot-app/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Message;
+import com.example.demo.repository.MessageRepository;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    private MessageRepository messageRepository;
+    @CrossOrigin(origins = "http://localhost:5173")
+    @PostMapping("/hello")
+    public Message saveMessage(@RequestBody Message message) {
+        return messageRepository.save(message);
+    }
+
+    @GetMapping("/hello")
+    public List<Message> getMessages() {
+        return messageRepository.findAll();
+    }
+}
+
---
+++ walnut/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import MessageComponent from './components/MessageComponent';
+function App() {
+  return (
+      <div className="App">
+          <header className="App-header">
+              <MessageComponent />
+          </header>
+      </div>
+  );
+}
+
+export default App
