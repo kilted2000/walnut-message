7456c7a4967b9cd6525b96140e97da9bf808a2bf
Richard <richardlwhittington@gmail.com>
2024-08-27 17:45:48 -0500

    add cors header

--- spring-boot-app/src/main/java/com/example/demo/controller/HelloController.java
+++ spring-boot-app/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,5 +1,4 @@
 package com.example.demo.controller;
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Message;
 import com.example.demo.repository.MessageRepository;
-
 @RestController
 public class HelloController {
 
@@ -22,10 +20,12 @@ public class HelloController {
     public Message saveMessage(@RequestBody Message message) {
         return messageRepository.save(message);
     }
-
+    @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping("/hello")
     public List<Message> getMessages() {
         return messageRepository.findAll();
     }
+
+
 }
 
