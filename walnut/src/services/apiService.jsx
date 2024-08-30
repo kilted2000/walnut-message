9829d52d2d70a70fde784ec9a71382e3adb2fd6d
Richard <richardlwhittington@gmail.com>
2024-08-30 00:06:50 -0500

    refactor code

--- walnut/src/services/apiService.jsx
+++ walnut/src/services/apiService.jsx
@@ -1,35 +1,4 @@
-// import axios from 'axios';
 
-// const API_URL = 'http://localhost:8080';
-
-// export const getMessages = async () => {
-//     try {
-//         const response = await axios.get(`${API_URL}/hello`, 
-//             {
-//                 headers: {
-//                     "Access-Control-Allow-Origin": "*",  
-//             },
-//         }
-//         )
-
-        
-      
-//         return response.data;
-//     } catch (error) {
-//         console.error('Error fetching notes:', error);
-//         throw error;
-//     }
-// };
-
-// export const saveMessage = async (content) => {
-//     try {
-//         const response = await axios.post(`${API_URL}/hello`, { content });
-//         return response.data;
-//     } catch (error) {
-//         console.error('Error saving notes:', error);
-//         throw error;
-//     }
-// };
  const API_URL = 'http://localhost:8080';
 
 export const getMessages = async () => {
